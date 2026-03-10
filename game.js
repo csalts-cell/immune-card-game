8b3f38799a6d3c10da9886e94d6d0f317b4ab38c
csalts-cell <carlos.salinas@pisd.edu>
2026-03-09 19:23:23 -0500

    Update game.js

--- game.js
+++ game.js
@@ -1,8 +1,12 @@
 function updateCard(){
 
 let name = document.getElementById("nameInput").value
+let marker = document.getElementById("markerInput").value
+let attack = document.getElementById("attackInput").value
+let defense = document.getElementById("defenseInput").value
 let hp = document.getElementById("hpInput").value
-let def = document.getElementById("defInput").value
+let ability = document.getElementById("abilityInput").value
+
 let color = document.getElementById("colorPicker").value
 let bg = document.getElementById("bgInput").value
 
@@ -10,8 +14,17 @@ if(name){
 document.getElementById("cardName").innerText = name
 }
 
+if(marker){
+document.getElementById("cardMarker").innerText = "Marker: " + marker
+}
+
+if(ability){
+document.getElementById("cardAbility").innerText = "Ability: " + ability
+}
+
+document.getElementById("attack").innerText = attack
+document.getElementById("defense").innerText = defense
 document.getElementById("hp").innerText = hp
-document.getElementById("def").innerText = def
 
 let card = document.getElementById("card")
 
